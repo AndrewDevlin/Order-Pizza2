3131f403b54c477776db172a87ea4a6ec9ce2db2
Andrew Devlin <andrewkdevlin23@gmail.com>
2017-01-28 14:05:39 -0800

    create a running total

--- order-pizza/js/scripts.js
+++ order-pizza/js/scripts.js
@@ -4,20 +4,21 @@ function Pizza(toppings, size) {
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  var sizePrice = 0
+  var sizePrice = 0;
   if (newPizza.pizzaSize === 'small') {
-    sizePrice = 5
+    sizePrice = 5;
   }
   else if (newPizza.pizzaSize === 'medium') {
-    sizePrice = 10
+    sizePrice = 10;
   }
   else {
-    sizePrice = 15
+    sizePrice = 15;
   }
   return this.pizzaToppings.length + sizePrice;
 }
 
 $(document).ready(function() {
+  var totalPrice = 0;
   $("form#order").submit(function(event) {
     event.preventDefault();
 
@@ -28,8 +29,11 @@ $(document).ready(function() {
     });
     var inputtedSize = $("input:radio[name=size]:checked").val();
     newPizza = new Pizza(inputtedToppings, inputtedSize);
+    totalPrice += newPizza.pizzaPrice();
 
     $("#your-order").show();
     $("#your-order").append("<li>" + "Size: " + newPizza.pizzaSize + "</li>" + "<li>" + "Toppings:" + newPizza.pizzaToppings + "</li>" + "<li>Total: $" + newPizza.pizzaPrice() + "</li>" + "<li>" + "</li>");
+
+    $("#running-total").text("Your total: $" + totalPrice);
   });
 });
